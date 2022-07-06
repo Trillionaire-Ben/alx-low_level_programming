dcbebfd2466954e803f0737020c7c9bbcea79a92
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-06 04:58:42 -0700

    last try on task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,16 @@
  *  * main - Check description
  *   * Description: It prints the word Holberton, followed by a new line.
  *    * Return: 0.
- *     */
-int main()
+*/
+int main(void)
 {
-	char try[8] = '_putchar';
+	char try[8] = "_putchar";
 	int i;
 
 	for (i = 0; i < 9; i++)
-		{
-			_putchar(try[i]);
-		}
-		_putchar('\n');
-	
+	{
+		_putchar(try[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
