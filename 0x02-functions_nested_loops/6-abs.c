d5c68b77f0b778ddb6c1c2f2ce801ab09019f15a
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-06 08:06:34 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - check description
+ * Description: to compute the absolute value of an integer
+ * @n: number to check
+ * Return: 0.
+*/
+
+int _abs(int n)
+{
+	int b;
+
+	n = abs(b);
+	_putchar(n);
+
+	return (0);
+}
