5837b5749c342ef7c8868f3ba8f9010edb35c655
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-06 07:42:51 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_sign - check description
+ * @n: character to check
+ * Description: print the sign and return value
+ * Return: 0 or 1 or -1.
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
