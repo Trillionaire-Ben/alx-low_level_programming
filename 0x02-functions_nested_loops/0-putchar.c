1a1d7c27cfc206a0a774ac27ac7dec7bcf785eee
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-06 05:47:36 -0700

    task 1 done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,6 @@
 /**
  *  * main - Check description
  *   * Description: It prints the word _putchar, followed by a new line.
- *    * Return: 0.
 */
 int main(void)
 {
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ *  * print_alphabet(void) - check description
+ *   *Description: print lower case letters a to z
+*/
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x < 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
