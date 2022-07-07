badd89a0b72b62c714c995127b0ed48175e5a6db
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-07 03:44:27 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check description
+ * Description: testing for positive_or_negative(0)
+ * Return: i0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
