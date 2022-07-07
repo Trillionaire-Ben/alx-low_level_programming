d11d22b067bec6b86d9e05eceaef23ddbc5fb33b
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-07 05:14:13 -0700

    the very last 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,7 +15,7 @@ int largest_number(int a, int b, int c)
 	if (a >= b && a >= c)
 	{
 		largest = a;
-	} else if (b >= a && a >= c)
+	} else if (b >= a && b >= c)
 	{
 		largest = b;
 	} else if (c >= a && c >= b)
