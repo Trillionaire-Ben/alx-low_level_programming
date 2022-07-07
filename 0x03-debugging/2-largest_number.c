2f725b9bd5ee1e6498e6ef039e02fd9e18ca6d83
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-07 04:50:54 -0700

    fixing final error on task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,13 +12,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && a > c) || (a > b && b > c) || (a > b && c > b))
+	if (a >= b && a >= c)
 	{
 		largest = a;
-	} else if ((b > a && a > c) || (b > a && b > c) || (b > c && c > a))
+	} else if (b >= a && a >= c)
 	{
 		largest = b;
-	} else
+	} else if (c >= a && c >= b)
 	{
 		largest = c;
 	}
