6755145e1d716dc05585fca8ffb6a98f9aff678a
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-09 08:05:29 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0
+*/
+int main(void)
+{
+	int num = 1;
+
+	for (; num < 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("Buzz\n");
+
+	return (0);
+}
