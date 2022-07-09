cd0418431c085817fec275ca6864f0dd793f55cc
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-09 07:12:32 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -12,9 +12,9 @@ int _isupper(int c)
 	char uppercase;
 	int isupper = 0;
 
-	for (uppercase = 'A'; uppercase <= 'z'; uppercase++)
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
 	{
-		if (c == uppercase)
+		if (uppercase == c)
 		{
 			isupper = 1;
 		}
