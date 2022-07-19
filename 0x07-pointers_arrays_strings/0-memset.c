1c52a2c495ea2805c54d9ae917332a88186980cf
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-07-19 12:45:31 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	while (i > 0)
+	while (n > 0)
 	{
 		s[i] = b;
 		i++;
@@ -23,4 +23,3 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return (s);
 }
-
