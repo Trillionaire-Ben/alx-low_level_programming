a8d5cdc4a08c5fa8ac0d9b48ffc0819d88288cf0
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-08-09 13:32:34 -0700

    unfinished 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _sqrt_recursion - fn to print the natural squareroot of a number
+ * @n: integer to find squareroot
+ * Return: nothing
+ */
+
+int _sqrt_recursion(int n)
+{
