49a37db3d557f4a9ac5c843dd3091adb0aa3dae0
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-08-09 12:55:29 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,4 +2,16 @@
 #include "main.h"
 
 /**
- *
+ * factorial - fn that returns the value of a given number
+ * @n: integer value
+ * Return: nothing
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
