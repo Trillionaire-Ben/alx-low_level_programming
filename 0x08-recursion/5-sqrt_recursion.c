9aa76776ebd42716cd4e972ea94470cda4516571
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-08-10 12:49:03 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,8 +4,31 @@
 /**
  * _sqrt_recursion - fn to print the natural squareroot of a number
  * @n: integer to find squareroot
- * Return: nothing
+ * Return: int
  */
 
 int _sqrt_recursion(int n)
 {
+	int _squareroot(int n, int i);
+
+	return (_squareroot(n, 1));
+}
+
+/**
+ * _squareroot - _sqrt_recursion
+ * @n: integer parameter
+ * @i: integer input
+ * Return: square root
+ */
+
+int _squareroot(int n, int i)
+{
+	if (n < 0)
+		return (-1);
+	/* the next line actually ensures the natural squareroot part.*/
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_squareroot(n, i + 1));
+}
