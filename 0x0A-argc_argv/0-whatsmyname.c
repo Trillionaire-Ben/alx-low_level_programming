e9a7a58981202a20e47e604e3d359379578081a1
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-08-13 07:52:41 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function
+ * @argc: argument constant
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("The name of the program is %s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This folder contains projects using argc and argv.
