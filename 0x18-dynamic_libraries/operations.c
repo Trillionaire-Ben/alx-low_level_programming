bb57fd68a279f45bbfd55b257b87832b48b65db7
Trillionaire-Ben <orumah.chinecherem@gmail.com>
2022-10-03 16:52:15 -0700

    task 3

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - fn
+ * @a: params
+ * @b: params
+ * Return: nothing
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - fn
+ * @a: params
+ * @b: params
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - fn
+ * @a: params
+ * @b: params
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a *b);
+}
+
+/**
+ * mod - fn
+ * @a: params
+ * @b: params
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
